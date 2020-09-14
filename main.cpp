e44a388a95ae9a62995a654d0a462968d0d80bd9
tonitoicik <54882998+tonitoicik@users.noreply.github.com>
2020-09-14 10:46:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  int a,b,c = 0;
+  int arr[2] = {0, 0};
+  cout <<" Милорд, введите первое, а затем второе число через пробел"<<endl;
+  cin >> a >> b; 
+  c=a;
+  a=b;
+  b=c;
+  cout<< "первое число теперь = "<< a <<endl<< "второе число теперь = " << b<<endl<< "А теперь по другому"<<endl;
+ 
 }
